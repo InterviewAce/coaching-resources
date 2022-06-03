93d4e82895e659591c3e18f59522f7a0f7903e07
shreshth <shreshthsrivastava2@gmail.com>
2022-06-02 21:55:49 -0700

    [solutions] Add traditional binary search solution for Sqrt X in JS.

--- solutions/graphs/binary-search/sqrtX.js
+++ solutions/graphs/binary-search/sqrtX.js
@@ -1,3 +1,25 @@
+/*
+Alternate solution (still needs some clean up):
+const mySqrt = (x) => {
+  let left = 1;
+  let right = x;
+  let result = 0;
+  
+  while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      const midSquared = mid ** 2;
+      
+      if (midSquared > x) right = mid - 1;
+      if (midSquared <= x) {
+          result = mid;
+          left = mid + 1;
+      }
+  }
+  return result;
+};
+
+*/
+
 const square = (x) => {
   return x * x;
 };
