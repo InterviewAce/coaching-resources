8f91d5a0d2d41a03042ccd61c94345948ad48171
shreshth <shreshthsrivastava2@gmail.com>
2022-06-04 15:30:21 -0700

    [solutions][dp] Add solution to Fibonacci Number in JS.

---
+++ solutions/dp/fibonacciNumber.js
@@ -0,0 +1,50 @@
+/*
+RECURSIVE SOLUTION (NO DP)
+*/
+const fib = (n) => {
+  if (n <= 1) return n;
+
+  return fib(n - 1) + fib(n - 2);
+};
+
+/*
+TOP-DOWN DP SOLUTION
+*/
+const fibHelper = (n, cache) => {
+  const nInCache = cache.hasOwnProperty(n);
+
+  if (nInCache) {
+    return cache[n];
+  }
+
+  const result = fibHelper(n - 1, cache) + fibHelper(n - 2, cache);
+  cache[n] = result;
+
+  return cache[n];
+};
+
+const fib = (n) => {
+  const cache = {};
+  cache[0] = 0;
+  cache[1] = 1;
+
+  return fibHelper(n, cache);
+};
+
+/*
+BOTTOM-UP DP SOLUTION
+*/
+const fib = (n) => {
+  const cache = {};
+
+  cache[0] = 0;
+  cache[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    const fibOfI = cache[i - 1] + cache[i - 2];
+
+    cache[i] = fibOfI;
+  }
+
+  return cache[n];
+};
