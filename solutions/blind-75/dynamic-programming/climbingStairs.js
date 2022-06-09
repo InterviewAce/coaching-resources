dbf8ff29b368b3f0982ce9c46249f5ec90cf58ba
shreshth <shreshthsrivastava2@gmail.com>
2022-06-09 16:50:30 -0700

    [solutions][blind-75] Reorganize blind 75 solutions into the correct categories.

---
+++ solutions/blind-75/dynamic-programming/climbingStairs.js
@@ -0,0 +1,62 @@
+/*
+TOP-DOWN DP SOLUTION
+*/
+const climbStairsHelper = (n, cache) => {
+	const nInCache = cache.hasOwnProperty(n);
+
+	if (nInCache) {
+		return cache[n];
+	}
+
+	const result =
+		climbStairsHelper(n - 1, cache) + climbStairsHelper(n - 2, cache);
+	cache[n] = result;
+
+	return cache[n];
+};
+
+const climbStairs = (n) => {
+	const cache = {};
+
+	cache[0] = 1;
+	cache[1] = 1;
+
+	return climbStairsHelper(n, cache);
+};
+
+/*
+BOTTOM-UP DP SOLUTION
+*/
+const climbStairs = (n) => {
+	const cache = {};
+
+	cache[0] = 1;
+	cache[1] = 1;
+
+	for (let i = 2; i <= n; i++) {
+		const numWaysToReachI = cache[i - 1] + cache[i - 2];
+
+		cache[i] = numWaysToReachI;
+	}
+
+	return cache[n];
+};
+
+/*
+BETTER BOTTOM-UP DP SOLUTION
+*/
+const climbStairs = (n) => {
+	if (n <= 2) return n;
+
+	let oneStepBack = 2;
+	let twoStepsBack = 1;
+	let totalWaysToReachI = 0;
+
+	for (let i = 2; i < n; i++) {
+		totalWaysToReachI = oneStepBack + twoStepsBack;
+		twoStepsBack = oneStepBack;
+		oneStepBack = totalWaysToReachI;
+	}
+
+	return totalWaysToReachI;
+};
