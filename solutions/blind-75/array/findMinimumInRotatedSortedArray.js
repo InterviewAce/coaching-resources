37f20d8b327b92f539974af65b98b9f27eef2a24
Rohin Manvi <rohinmanvi@DN51qo3t.SUNet>
2022-06-09 11:18:36 -0700

    added

---
+++ solutions/blind-75/array/findMinimumInRotatedSortedArray.js
@@ -0,0 +1,16 @@
+const findMin = (nums) => {
+    let leftIdx = 0;
+    let rightIdx = nums.length - 1;
+  
+    while (leftIdx < rightIdx) {
+        let mid = leftIdx + Math.floor((rightIdx - leftIdx) / 2);
+
+        if (nums[mid] > nums[rightIdx]) {
+            leftIdx = mid + 1;
+        } else {
+            rightIdx = mid;
+        }
+    }
+    
+    return nums[leftIdx];
+};
