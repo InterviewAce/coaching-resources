e44f7728e6a40ffab83570bda4fe1f5805f1479a
Rohin Manvi <rohinmanvi@DN51qo3t.SUNet>
2022-06-09 12:15:54 -0700

    a

--- solutions/dp/coinChange.js
+++ solutions/dp/coinChange.js
@@ -4,48 +4,48 @@ TOP-DOWN DP SOLUTION
 const CANNOT_PRODUCE_SPECIFIED_AMOUNT = -1;
 
 const coinChangeHelper = (coins, amount, cache) => {
-  const amountInCache = cache.hasOwnProperty(amount);
+	const amountInCache = cache.hasOwnProperty(amount);
 
-  if (amountInCache) {
-    return cache[amount];
-  }
+	if (amountInCache) {
+		return cache[amount];
+	}
 
-  let fewestCoinsToMakeAmount = Infinity;
+	let fewestCoinsToMakeAmount = Infinity;
 
-  coins.forEach((coinAmount) => {
-    const remainingAmount = amount - coinAmount;
+	coins.forEach((coinAmount) => {
+		const remainingAmount = amount - coinAmount;
 
-    if (remainingAmount < 0) return;
+		if (remainingAmount < 0) return;
 
-    const remainingCoinsRequired = coinChangeHelper(
-      coins,
-      remainingAmount,
-      cache,
-    );
+		const remainingCoinsRequired = coinChangeHelper(
+			coins,
+			remainingAmount,
+			cache,
+		);
 
-    if (remainingCoinsRequired === CANNOT_PRODUCE_SPECIFIED_AMOUNT) return;
+		if (remainingCoinsRequired === CANNOT_PRODUCE_SPECIFIED_AMOUNT) return;
 
-    fewestCoinsToMakeAmount = Math.min(
-      fewestCoinsToMakeAmount,
-      1 + remainingCoinsRequired,
-    );
-  });
+		fewestCoinsToMakeAmount = Math.min(
+			fewestCoinsToMakeAmount,
+			1 + remainingCoinsRequired,
+		);
+	});
 
-  if (fewestCoinsToMakeAmount === Infinity) {
-    fewestCoinsToMakeAmount = CANNOT_PRODUCE_SPECIFIED_AMOUNT;
-  }
+	if (fewestCoinsToMakeAmount === Infinity) {
+		fewestCoinsToMakeAmount = CANNOT_PRODUCE_SPECIFIED_AMOUNT;
+	}
 
-  cache[amount] = fewestCoinsToMakeAmount;
+	cache[amount] = fewestCoinsToMakeAmount;
 
-  return cache[amount];
+	return cache[amount];
 };
 
 const coinChange = (coins, amount) => {
-  const cache = {};
+	const cache = {};
 
-  cache[0] = 0;
+	cache[0] = 0;
 
-  return coinChangeHelper(coins, amount, cache);
+	return coinChangeHelper(coins, amount, cache);
 };
 
 /*
@@ -54,38 +54,38 @@ BOTTOM-UP DP SOLUTION
 const CANNOT_PRODUCE_SPECIFIED_AMOUNT = -1;
 
 const coinChange = (coins, targetAmount) => {
-  const cache = {};
+	const cache = {};
 
-  cache[0] = 0;
+	cache[0] = 0;
 
-  for (let curAmount = 1; curAmount <= targetAmount; curAmount++) {
-    let fewestCoinsToMakeCurAmount = Infinity;
+	for (let curAmount = 1; curAmount <= targetAmount; curAmount++) {
+		let fewestCoinsToMakeCurAmount = Infinity;
 
-    coins.forEach((coinAmount) => {
-      const remainingAmount = curAmount - coinAmount;
+		coins.forEach((coinAmount) => {
+			const remainingAmount = curAmount - coinAmount;
 
-      if (remainingAmount < 0) return;
+			if (remainingAmount < 0) return;
 
-      // If `remainingAmount` is in `cache`, set `minCoinsToMakeRemainingAmount` to `cache[remainingAmount]. Otherwise, set it to `Infinity`.
-      const remainingAmountInCache = cache.hasOwnProperty(remainingAmount);
-      const minCoinsToMakeRemainingAmount = remainingAmountInCache
-        ? cache[remainingAmount]
-        : Infinity;
+			// If `remainingAmount` is in `cache`, set `minCoinsToMakeRemainingAmount` to `cache[remainingAmount]. Otherwise, set it to `Infinity`.
+			const remainingAmountInCache = cache.hasOwnProperty(remainingAmount);
+			const minCoinsToMakeRemainingAmount = remainingAmountInCache
+				? cache[remainingAmount]
+				: Infinity;
 
-      fewestCoinsToMakeCurAmount = Math.min(
-        fewestCoinsToMakeCurAmount,
-        1 + minCoinsToMakeRemainingAmount,
-      );
-    });
+			fewestCoinsToMakeCurAmount = Math.min(
+				fewestCoinsToMakeCurAmount,
+				1 + minCoinsToMakeRemainingAmount,
+			);
+		});
 
-    cache[curAmount] = fewestCoinsToMakeCurAmount;
-  }
+		cache[curAmount] = fewestCoinsToMakeCurAmount;
+	}
 
-  const result = cache[targetAmount];
+	const result = cache[targetAmount];
 
-  if (result === Infinity) {
-    return CANNOT_PRODUCE_SPECIFIED_AMOUNT;
-  }
+	if (result === Infinity) {
+		return CANNOT_PRODUCE_SPECIFIED_AMOUNT;
+	}
 
-  return result;
+	return result;
 };
--- solutions/dp/fibonacciNumber.js
+++ solutions/dp/fibonacciNumber.js
@@ -2,68 +2,68 @@
 RECURSIVE SOLUTION (NO DP)
 */
 const fib = (n) => {
-  if (n <= 1) return n;
+	if (n <= 1) return n;
 
-  return fib(n - 1) + fib(n - 2);
+	return fib(n - 1) + fib(n - 2);
 };
 
 /*
 TOP-DOWN DP SOLUTION
 */
 const fibHelper = (n, cache) => {
-  const nInCache = cache.hasOwnProperty(n);
+	const nInCache = cache.hasOwnProperty(n);
 
-  if (nInCache) {
-    return cache[n];
-  }
+	if (nInCache) {
+		return cache[n];
+	}
 
-  const result = fibHelper(n - 1, cache) + fibHelper(n - 2, cache);
-  cache[n] = result;
+	const result = fibHelper(n - 1, cache) + fibHelper(n - 2, cache);
+	cache[n] = result;
 
-  return cache[n];
+	return cache[n];
 };
 
 const fib = (n) => {
-  const cache = {};
-  cache[0] = 0;
-  cache[1] = 1;
+	const cache = {};
+	cache[0] = 0;
+	cache[1] = 1;
 
-  return fibHelper(n, cache);
+	return fibHelper(n, cache);
 };
 
 /*
 BOTTOM-UP DP SOLUTION
 */
 const fib = (n) => {
-  const cache = {};
+	const cache = {};
 
-  cache[0] = 0;
-  cache[1] = 1;
+	cache[0] = 0;
+	cache[1] = 1;
 
-  for (let i = 2; i <= n; i++) {
-    const fibOfI = cache[i - 1] + cache[i - 2];
+	for (let i = 2; i <= n; i++) {
+		const fibOfI = cache[i - 1] + cache[i - 2];
 
-    cache[i] = fibOfI;
-  }
+		cache[i] = fibOfI;
+	}
 
-  return cache[n];
+	return cache[n];
 };
 
 /*
 BETTER BOTTOM-UP DP SOLUTION
 */
 const fib = (n) => {
-  if (n <= 1) return n;
+	if (n <= 1) return n;
 
-  let curFib = 0;
-  let lastFib = 1;
-  let lastLastFib = 0;
+	let curFib = 0;
+	let lastFib = 1;
+	let lastLastFib = 0;
 
-  for (let i = 2; i <= n; i++) {
-    curFib = lastFib + lastLastFib;
-    lastLastFib = lastFib;
-    lastFib = curFib;
-  }
-  
-  return curFib;
+	for (let i = 2; i <= n; i++) {
+		curFib = lastFib + lastLastFib;
+		lastLastFib = lastFib;
+		lastFib = curFib;
+	}
+
+	return curFib;
 };
