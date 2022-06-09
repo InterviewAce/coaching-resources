8a1981c3982e39b6f9b25fa9880eedd982f657e9
shreshth <shreshthsrivastava2@gmail.com>
2022-06-09 11:14:02 -0700

    [solutions][blind-75] Add Maximum Subarray solution in JS.

---
+++ solutions/blind-75/array/maximumSubarray.js
@@ -0,0 +1,22 @@
+/*
+We are only considering continguous subarrays. Thus, we consider our "current subarray" and we continuously expand this, tracking our sum as we go. If we ever hit a point where just taking the current number is better than taking our current subarray + the current number, then we reset our current subarray to just include the current number. For example:
+say our current subarray was [-4,5,-2] and our current number (the one that's after -2 in the input `nums`) is 10. We could let [-4,5,-2,10] be our subarray (this has a sum of 9) or we could let [10] be our subarray (which has a sum of 10). So, in this case, we discard the current subarray we've been tracking and create a new subarray that just contains 10, our current number.
+
+Using this approach, for each number, we compute the max subarray that could be created using the current number (only including numbers that occur before that number or itself). Then, we track these values over time, which gives us the max subarray sum.
+*/
+const maxSubArray = (nums) => {
+  let maxSubarraySum = -Infinity;
+  let curSubarraySum = 0;
+
+  for (const num of nums) {
+    if (curSubarraySum + num > num) {
+      curSubarraySum += num;
+    } else {
+      curSubarraySum = num;
+    }
+
+    maxSubarraySum = Math.max(maxSubarraySum, curSubarraySum);
+  }
+
+  return maxSubarraySum;
+};
