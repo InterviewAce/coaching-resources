5c504c4fef2a2af9f8b859bab2e53945d53a3c93
Rohin Manvi <rohinmanvi@DN51qo3t.SUNet>
2022-06-09 12:51:40 -0700

    a

--- solutions/blind-75/array/findMinimumInRotatedSortedArray.js
+++ solutions/blind-75/array/findMinimumInRotatedSortedArray.js
@@ -2,18 +2,15 @@
 binary search for point when the array decreases
 */
 const findMin = (nums) => {
-    let leftIdx = 0;
-    let rightIdx = nums.length - 1;
-  
-    while (leftIdx < rightIdx) {
+    leftIdx = 0;
+    rightIdx = nums.length - 1;
+    
+    while (leftIdx <= rightIdx) {
         const midIdx = leftIdx + Math.floor((rightIdx - leftIdx) / 2);
-
-        if (nums[midIdx] > nums[rightIdx]) {
-            leftIdx = midIdx + 1;
-        } else {
-            rightIdx = midIdx;
-        }
+        
+        if (nums[midIdx] <= nums[nums.length - 1]) rightIdx = midIdx - 1;
+        else leftIdx = midIdx + 1;
     }
     
     return nums[leftIdx];
-};
+}
