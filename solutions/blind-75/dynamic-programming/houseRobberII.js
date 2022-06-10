a5cee950359dae5874c9b59c8fcd28947345896e
shreshth <shreshthsrivastava2@gmail.com>
2022-06-09 17:59:35 -0700

    [solutions][blind-75] Add solution to Decode Ways in JS.

---
+++ solutions/blind-75/dynamic-programming/houseRobberII.js
@@ -0,0 +1,29 @@
+const robHelper = (nums, startIdx) => {
+  let previousTotalRobbed = 0;
+  let currentTotalRobbed = 0;
+
+  const numHousesAccountingForPotentialSkips = nums.length - 1 + startIdx;
+  for (let i = startIdx; i < numHousesAccountingForPotentialSkips; i++) {
+    const money = nums[i];
+
+    const temp = currentTotalRobbed;
+    currentTotalRobbed = Math.max(
+      previousTotalRobbed + money,
+      currentTotalRobbed,
+    );
+    previousTotalRobbed = temp;
+  }
+
+  return currentTotalRobbed;
+};
+
+/*
+BOTTOM-UP DP SOLUTION
+*/
+const rob = (nums) => {
+  if (nums.length === 1) return nums[0];
+
+  const maxMoneyIfSkipFirstHouse = robHelper(nums, 1);
+  const maxMoneyIfDontSkipFirstHouse = robHelper(nums, 0);
+  return Math.max(maxMoneyIfSkipFirstHouse, maxMoneyIfDontSkipFirstHouse);
+};
