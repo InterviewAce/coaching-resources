24bb59627b206b6d07f6d90811f09362811e5546
Rohin Manvi <rohinmanvi@Rohins-MacBook-Pro.local>
2022-06-19 03:07:27 +0000

    added validBinarySearchTree

---
+++ solutions/blind-75/tree/validBinarySearchTree.js
@@ -0,0 +1,26 @@
+const isValidBST = (root) => {
+    const stack = [];
+    
+    let previousValue = null;
+    
+    // We use iterative inorder traversal of the BST
+    while (stack.length || root) {
+        
+        while (root) {
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        
+        // We check if the next element in inorder traversal is smaller than the previous one
+        if (previousValue != null) {
+            const inOrder = root.val > previousValue;
+            if (!inOrder) return false;
+        }
+        previousValue = root.val;
+        
+        root = root.right
+    }
+    
+    return true;
+};
