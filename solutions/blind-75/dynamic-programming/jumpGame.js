02aa56c38e8a38f82b094471b35539a8a309292b
shreshth <shreshthsrivastava2@gmail.com>
2022-06-21 18:20:49 -0400

    [solutions][dp] Clean up Jump Game JS solution.

--- solutions/blind-75/dynamic-programming/jumpGame.js
+++ solutions/blind-75/dynamic-programming/jumpGame.js
@@ -1,20 +1,17 @@
-const createArrayOfSize = (arraySize, fillValue) => {
-  return new Array(arraySize, fillValue);
+const START_LOCATION = 0;
+
+const createArrayOfSize = (size, fillValue) => {
+  return new Array(size).fill(fillValue);
 };
 
 const canJump = (nums) => {
   const canReach = createArrayOfSize(nums.length, false);
-
-  canReach[0] = true;
+  canReach[START_LOCATION] = true;
 
   for (let i = 1; i < nums.length; i++) {
-    canReach[i] = false;
-
     for (let j = 0; j < i; j++) {
       const canReachJ = canReach[j];
-
-      const jJumpSize = nums[j];
-      const canReachIFromJ = j + jJumpSize >= i;
+      const canReachIFromJ = j + nums[j] >= i;
 
       if (canReachJ && canReachIFromJ) {
         canReach[i] = true;
