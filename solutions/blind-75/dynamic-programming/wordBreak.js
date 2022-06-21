6aedf6c21d10e132f2bd2777909d43d10955d7d3
shreshth <shreshthsrivastava2@gmail.com>
2022-06-21 18:20:24 -0400

    [solutions][dp] Clean up Word Break JS solution.

--- solutions/blind-75/dynamic-programming/wordBreak.js
+++ solutions/blind-75/dynamic-programming/wordBreak.js
@@ -11,42 +11,53 @@ TOP-DOWN DP SOLUTION
  * @return {Boolean} whether or not the string can be broken up into words
  */
 const wordBreakHelper = (s, wordSet, start, cache) => {
-    if (cache.has(start)) return false;
-    if (start === s.length) return true;
+  if (cache.has(start)) return false;
+  if (start === s.length) return true;
 
-    for (let end = start + 1; end < s.length + 1; end++) {
-        const potentialWord = s.slice(start, end);
+  for (let end = start + 1; end < s.length + 1; end++) {
+    const potentialWord = s.slice(start, end);
 
-        if (wordSet.has(potentialWord)) {
-            if (wordBreakHelper(s, wordSet, end, cache)) return true;
-        }
+    if (wordSet.has(potentialWord)) {
+      if (wordBreakHelper(s, wordSet, end, cache)) return true;
     }
+  }
 
-    cache.add(start);
-    return false;
+  cache.add(start);
+  return false;
 };
 
 const wordBreak = (s, wordDict) => {
-    const wordSet = new Set(wordDict);
-    return wordBreakHelper(s, wordSet, 0, new Set());
+  const wordSet = new Set(wordDict);
+  return wordBreakHelper(s, wordSet, 0, new Set());
 };
 
 /*
 BOTTOM-UP DP SOLUTION
 */
+const createArrayOfSize = (size, fillValue) => {
+  return new Array(size).fill(fillValue);
+};
+
 const wordBreak = (s, wordDict) => {
-    const wordSet = new Set(wordDict);
-    const cache = new Array(s.length + 1).fill(false);
-    cache[0] = true;
+  const validWords = new Set(wordDict);
 
-    for (let end = 1; end <= s.length; end++) {
-        for (let start = 0; start < end; start++) {
-            const w = s.slice(start, end);
-            if (cache[start] && wordSet.has(w)) {
-                cache[end] = true;
-            }
-        }
+  const canSplit = createArrayOfSize(s.length + 1, false);
+  canSplit[0] = true;
+
+  for (let endIdx = 1; endIdx <= s.length; endIdx++) {
+    for (let startIdx = 0; startIdx < endIdx; startIdx++) {
+      const canSplitUpThroughStart = canSplit[startIdx];
+
+      const remainingSubstring = s.substring(startIdx, endIdx);
+      const remainingSubstringInWordDict = validWords.has(remainingSubstring);
+
+      if (canSplitUpThroughStart && remainingSubstringInWordDict) {
+        canSplit[endIdx] = true;
+        break;
+      }
     }
+  }
 
-    return cache[s.length];
+  const lastIdx = s.length;
+  return canSplit[lastIdx];
 };
