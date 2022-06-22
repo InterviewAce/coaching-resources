33363ab719516d37e96ca46bfbd7b61cdc074a1b
Rohin Manvi <rohinmanvi@Rohins-MacBook-Pro.local>
2022-06-22 18:58:21 -0400

    update

--- solutions/blind-75/dynamic-programming/longestCommonSubsequence.js
+++ solutions/blind-75/dynamic-programming/longestCommonSubsequence.js
@@ -1,13 +1,13 @@
 var longestCommonSubsequence = function(textOne, textTwo) {
     const cache = {};
-    return longestCommonSubsequenceHelper(textOne, textTwo, 0, 0, cache);
+    const lastIndexOne = textOne.length - 1;
+    const lastIndexTwo = textTwo.length - 1;
+    return longestCommonSubsequenceHelper(textOne, textTwo, lastIndexOne, lastIndexTwo, cache);
 };
 
 function longestCommonSubsequenceHelper(textOne, textTwo, indexOne, indexTwo, cache) {
-    const lastIndexOne = textOne.length - 1;
-    const lastIndexTwo = textTwo.length - 1;
-    if (indexOne > lastIndexOne) return 0;
-    if (indexTwo > lastIndexTwo) return 0;
+    if (indexOne < 0) return 0;
+    if (indexTwo < 0) return 0;
     
     const key = indexOne + ' ' + indexTwo;
     if (cache.hasOwnProperty(key)) return cache[key];
@@ -19,11 +19,11 @@ function longestCommonSubsequenceHelper(textOne, textTwo, indexOne, indexTwo, ca
     const charsSame = charOne == charTwo;
 
     if (charsSame) {
-        const skipBothChars = longestCommonSubsequenceHelper(textOne, textTwo, indexOne + 1, indexTwo + 1, cache);
+        const skipBothChars = longestCommonSubsequenceHelper(textOne, textTwo, indexOne - 1, indexTwo - 1, cache);
         result = skipBothChars + 1;
     } else {
-        const skipCharOne = longestCommonSubsequenceHelper(textOne, textTwo, indexOne + 1, indexTwo, cache);
-        const skipCharTwo = longestCommonSubsequenceHelper(textOne, textTwo, indexOne, indexTwo + 1, cache);
+        const skipCharOne = longestCommonSubsequenceHelper(textOne, textTwo, indexOne - 1, indexTwo, cache);
+        const skipCharTwo = longestCommonSubsequenceHelper(textOne, textTwo, indexOne, indexTwo - 1, cache);
         result = Math.max(skipCharOne, skipCharTwo);
     }
     
