b45fb17942e8ddadf30aab5b0bb987b3add23edf
shreshth <shreshthsrivastava2@gmail.com>
2022-07-07 21:04:39 -0400

    [solutions][bfs] Clean up Employee Importance JS solution.

--- solutions/graphs/bfs/employeeImportance.js
+++ solutions/graphs/bfs/employeeImportance.js
@@ -1,31 +1,42 @@
-const GetImportance = function (employees, id) {
-  const idToEmployee = {};
+const buildEmployeeMap = (employees) => {
+  const idToEmployeeMap = {};
 
   for (let i = 0; i < employees.length; i++) {
     const curEmployee = employees[i];
-    idToEmployee[curEmployee.id] = curEmployee;
+    idToEmployeeMap[curEmployee.id] = curEmployee;
   }
 
+  return idToEmployeeMap;
+};
+
+const getTotalImportance = (rootId, idToEmployeeMap) => {
   let totalImportance = 0;
 
   const queue = new Queue();
-  queue.enqueue(id);
+  queue.enqueue(rootId);
 
   while (queue.size() > 0) {
     // Remove node
     const employeeId = queue.dequeue();
 
     // Process node
-    const employee = idToEmployee[employeeId];
+    const employee = idToEmployeeMap[employeeId];
     const { id, importance, subordinates } = employee;
 
     totalImportance += importance;
 
     // Add neighbors
     subordinates.forEach((subordinate) => {
+      // This is a DAG (directed acylclic graph) meaning we do not need any visited checks
       queue.enqueue(subordinate);
     });
   }
 
   return totalImportance;
+};
+
+const GetImportance = function (employees, id) {
+  const idToEmployeeMap = buildEmployeeMap(employees);
+
+  return getTotalImportance(id, idToEmployeeMap);
 };
