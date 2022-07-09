6331561c78292e250551c96ab7881b55fae85bd9
shreshth <shreshthsrivastava2@gmail.com>
2022-07-09 18:28:53 -0400

    [solutions][binary-search] Clean up Valid Perfect Square JS solution.

--- solutions/binary-search/validPerfectSquare.js
+++ solutions/binary-search/validPerfectSquare.js
@@ -1,8 +1,8 @@
-const getMid = (leftIdx, rightIdx) => {
-  /*
+const getMidNum = (leftNum, rightNum) => {
+    /*
   Why do we use this math to find the middle index?
   Couldn't we just use
-    const midIdx = Math.floor((left + right) / 2);
+    const midNum = Math.floor((left + right) / 2);
     
   The reason is integer overflow. In JavaScript, the maximum
   size of a number is (2^53) - 1. Suppose we used the above logic
@@ -23,28 +23,23 @@ const getMid = (leftIdx, rightIdx) => {
   puts as at the middle index. But, there is no risk of computing
   numbers that cause integer overflow.
   */
-  const midIdx = leftIdx + Math.floor((rightIdx - leftIdx) / 2);
+    const midNum = leftNum + Math.floor((rightNum - leftNum) / 2);
 
-  return midIdx;
-};
-
-const square = (x) => {
-  return x * x;
+    return midNum;
 };
 
 const isPerfectSquare = (num) => {
-  let lowerBound = 0;
-  let upperBound = num;
+    let lowerBound = 0;
+    let upperBound = num;
 
-  while (lowerBound <= upperBound) {
-    const mid = getMid(lowerBound, upperBound);
+    while (lowerBound <= upperBound) {
+        const midNum = getMidNum(lowerBound, upperBound);
+        const midNumSquared = midNum * midNum;
 
-    const midSquared = square(mid);
+        if (midNumSquared === num) return true;
+        if (midNumSquared < num) lowerBound = midNum + 1;
+        if (midNumSquared > num) upperBound = midNum - 1;
+    }
 
-    if (midSquared === num) return true;
-    if (midSquared < num) lowerBound = mid + 1;
-    if (midSquared > num) upperBound = mid - 1;
-  }
-
-  return false;
+    return false;
 };
