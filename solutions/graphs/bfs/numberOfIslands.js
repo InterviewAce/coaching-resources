334709d59dab357b5bcf26d385d6e1515cd4a61f
shreshth <shreshthsrivastava2@gmail.com>
2022-07-10 19:53:09 -0400

    [solutions][bfs] Add visited set explanation for Number of Islands BFS JS.

--- solutions/graphs/bfs/numberOfIslands.js
+++ solutions/graphs/bfs/numberOfIslands.js
@@ -1,69 +1,75 @@
 const LAND = '1';
 
 const createArrayOfSize = (numRows, numCols) => {
-  const arr = new Array(numRows);
+    const arr = new Array(numRows);
 
-  for (let i = 0; i < numRows; i++) {
-    arr[i] = new Array(numCols);
-  }
+    for (let i = 0; i < numRows; i++) {
+        arr[i] = new Array(numCols);
+    }
 
-  return arr;
+    return arr;
 };
 
 const isInBounds = (grid, row, col) => {
-  const numRows = grid.length;
-  const numCols = grid[0].length;
+    const numRows = grid.length;
+    const numCols = grid[0].length;
 
-  if (row < 0 || row >= numRows) return false;
-  if (col < 0 || col >= numCols) return false;
+    if (row < 0 || row >= numRows) return false;
+    if (col < 0 || col >= numCols) return false;
 
-  return true;
+    return true;
 };
 
 const markWholeIslandAsVisited = (grid, startRow, startCol, visited) => {
-  const queue = new Queue();
-  queue.enqueue([startRow, startCol]);
+    const queue = new Queue();
+    queue.enqueue([startRow, startCol]);
 
-  while (queue.size() > 0) {
-    // Remove node
-    const [row, col] = queue.dequeue();
+    while (queue.size() > 0) {
+        // Remove node
+        const [row, col] = queue.dequeue();
 
-    // Process node
-    if (!isInBounds(grid, row, col)) continue;
-    if (visited[row][col]) continue;
-    if (grid[row][col] != LAND) continue; // Note this must come AFTER the isInBounds check
+        // Process node
+        if (!isInBounds(grid, row, col)) continue;
 
-    visited[row][col] = true;
+        // This is an undirected graph. From [0,0] we can go to [0,1] and from
+        // [0,1] we can go to [0,0].
+        // Thus, this graph CAN have cycles (all undirected graphs can have cycles),
+        // so we must tracked visited nodes to prevent infinite loops.
+        if (visited[row][col]) continue;
 
-    // Add neighbors
-    queue.enqueue([row - 1, col]); // up
-    queue.enqueue([row + 1, col]); // down
-    queue.enqueue([row, col - 1]); // left
-    queue.enqueue([row, col + 1]); // right
-  }
+        if (grid[row][col] != LAND) continue; // Note this must come AFTER the isInBounds check
+
+        visited[row][col] = true;
+
+        // Add neighbors
+        queue.enqueue([row - 1, col]); // up
+        queue.enqueue([row + 1, col]); // down
+        queue.enqueue([row, col - 1]); // left
+        queue.enqueue([row, col + 1]); // right
+    }
 };
 
 const numIslands = function (grid) {
-  const numRows = grid.length;
+    const numRows = grid.length;
 
-  if (numRows == 0) return 0;
-  const numCols = grid[0].length;
+    if (numRows == 0) return 0;
+    const numCols = grid[0].length;
 
-  let numberOfIslands = 0;
-  const visited = createArrayOfSize(numRows, numCols);
+    let numberOfIslands = 0;
+    const visited = createArrayOfSize(numRows, numCols);
 
-  for (let row = 0; row < numRows; row++) {
-    for (let col = 0; col < numCols; col++) {
-      const terrainType = grid[row][col];
-      const isLand = terrainType === LAND;
+    for (let row = 0; row < numRows; row++) {
+        for (let col = 0; col < numCols; col++) {
+            const terrainType = grid[row][col];
+            const isLand = terrainType === LAND;
 
-      if (!isLand) continue;
-      if (visited[row][col]) continue;
+            if (!isLand) continue;
+            if (visited[row][col]) continue;
 
-      numberOfIslands++;
-      markWholeIslandAsVisited(grid, row, col, visited);
+            numberOfIslands++;
+            markWholeIslandAsVisited(grid, row, col, visited);
+        }
     }
-  }
 
-  return numberOfIslands;
+    return numberOfIslands;
 };
