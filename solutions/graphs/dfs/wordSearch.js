3d769c0784e4a79faae122f25ee8a2830aed8cd8
shreshth <shreshthsrivastava2@gmail.com>
2022-07-10 19:57:57 -0400

    [solutions][dfs] Add visited set explanation for Word Ladder DFS JS.

--- solutions/graphs/dfs/wordSearch.js
+++ solutions/graphs/dfs/wordSearch.js
@@ -1,87 +1,93 @@
 const directions = [
-  [0, 1],
-  [1, 0],
-  [0, -1],
-  [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
 ];
 
 const isValidPosition = (grid, row, col, visited, curPosition) => {
-  const numRows = grid.length;
-  const numCols = grid[0].length;
+    const numRows = grid.length;
+    const numCols = grid[0].length;
 
-  const rowInBounds = row >= 0 && row < numRows;
-  const colInBounds = col >= 0 && col < numCols;
+    const rowInBounds = row >= 0 && row < numRows;
+    const colInBounds = col >= 0 && col < numCols;
 
-  if (!rowInBounds || !colInBounds) return false;
-  if (visited.has(curPosition)) return false;
+    if (!rowInBounds || !colInBounds) return false;
 
-  return true;
+    // This is an undirected graph. From the letter at position [1,3],
+    // we can go to the position [2,3] and from the position [2,3], we
+    // can go to the position [1,3].
+    // Thus, this graph CAN have cycles (all undirected graphs can have cycles),
+    // so we must tracked visited nodes to prevent infinite loops.
+    if (visited.has(curPosition)) return false;
+
+    return true;
 };
 
 const searchForWordSubstring = (word, wordIdx, row, col, board, visited) => {
-  const curPosition = `${row}, ${col}`;
+    const curPosition = `${row}, ${col}`;
 
-  // Base cases
-  const wordIsFinished = wordIdx >= word.length;
-  if (wordIsFinished) return true;
+    // Base cases
+    const wordIsFinished = wordIdx >= word.length;
+    if (wordIsFinished) return true;
 
-  if (!isValidPosition(board, row, col, visited, curPosition)) return false;
+    if (!isValidPosition(board, row, col, visited, curPosition)) return false;
 
-  // Process node
-  const curBoardChar = board[row][col];
-  const curCharInWord = word[wordIdx];
+    // Process node
+    const curBoardChar = board[row][col];
+    const curCharInWord = word[wordIdx];
 
-  if (curBoardChar !== curCharInWord) return false;
+    if (curBoardChar !== curCharInWord) return false;
 
-  visited.add(curPosition);
+    visited.add(curPosition);
 
-  // Recurse on neighbors
+    // Recurse on neighbors
 
-  // Try going up, left, down, and right
-  for (const direction of directions) {
-    const [rowChange, colChange] = direction;
+    // Try going up, left, down, and right
+    for (const direction of directions) {
+        const [rowChange, colChange] = direction;
 
-    const newRow = row + rowChange;
-    const newCol = col + colChange;
+        const newRow = row + rowChange;
+        const newCol = col + colChange;
 
-    // If the next square that we're looking at contains the remaining substring of `word`, then we've found our answer. For example, suppose word is "abcdef" and we have found "a", "b", "c" (so the letter we're currently on is "c"). Then, if we go one of our 4 directions and find that the position can form "def" without revisiting any of our visited squares, then we know we can form "abcdef" (since we already found "abc").
-    const remainingWordSubstringExists = searchForWordSubstring(
-      word,
-      wordIdx + 1,
-      newRow,
-      newCol,
-      board,
-      visited,
-    );
+        // If the next square that we're looking at contains the remaining substring of `word`, then we've found our answer. For example, suppose word is "abcdef" and we have found "a", "b", "c" (so the letter we're currently on is "c"). Then, if we go one of our 4 directions and find that the position can form "def" without revisiting any of our visited squares, then we know we can form "abcdef" (since we already found "abc").
+        const remainingWordSubstringExists = searchForWordSubstring(
+            word,
+            wordIdx + 1,
+            newRow,
+            newCol,
+            board,
+            visited,
+        );
 
-    if (remainingWordSubstringExists) return true;
-  }
+        if (remainingWordSubstringExists) return true;
+    }
 
-  visited.delete(curPosition);
+    visited.delete(curPosition);
 
-  return false;
+    return false;
 };
 
 const exist = (board, word) => {
-  const numRows = board.length;
-  const numCols = board[0].length;
+    const numRows = board.length;
+    const numCols = board[0].length;
 
-  const visited = new Set();
+    const visited = new Set();
 
-  for (let row = 0; row < numRows; row++) {
-    for (let col = 0; col < numCols; col++) {
-      const foundWord = searchForWordSubstring(
-        word,
-        0,
-        row,
-        col,
-        board,
-        visited,
-      );
+    for (let row = 0; row < numRows; row++) {
+        for (let col = 0; col < numCols; col++) {
+            const foundWord = searchForWordSubstring(
+                word,
+                0,
+                row,
+                col,
+                board,
+                visited,
+            );
 
-      if (foundWord) return true;
+            if (foundWord) return true;
+        }
     }
-  }
 
-  return false;
+    return false;
 };
