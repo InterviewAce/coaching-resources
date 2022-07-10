3e5d30a5cf153b2c585b2d1fc23eada80931d391
shreshth <shreshthsrivastava2@gmail.com>
2022-07-10 19:59:13 -0400

    [solutions][dfs] Add visited set explanation for Number of Provinces DFS JS.

--- solutions/graphs/dfs/numberOfProvinces.js
+++ solutions/graphs/dfs/numberOfProvinces.js
@@ -1,40 +1,46 @@
 const CONNECTED = 1;
 
 const markProvinceAsVisited = (visited, city, isConnected) => {
-  // For this problem, our base case in automatically handled
-  // (our base case occurs when the current city has 0 unvisited
-  // neighbor cities).
+    // For this problem, our base case in automatically handled
+    // (our base case occurs when the current city has 0 unvisited
+    // neighbor cities).
 
-  // Process node
-  visited.add(city);
+    // Process node
+    visited.add(city);
 
-  // Recurse on neighbors
-  for (
-    let potentialNeighborCity = 0;
-    potentialNeighborCity < isConnected.length;
-    potentialNeighborCity++
-  ) {
-    const isNeighbor = isConnected[city][potentialNeighborCity] === CONNECTED;
+    // Recurse on neighbors
+    for (
+        let potentialNeighborCity = 0;
+        potentialNeighborCity < isConnected.length;
+        potentialNeighborCity++
+    ) {
+        const isNeighbor =
+            isConnected[city][potentialNeighborCity] === CONNECTED;
 
-    if (!isNeighbor) continue;
-    if (visited.has(potentialNeighborCity)) continue;
+        if (!isNeighbor) continue;
 
-    markProvinceAsVisited(visited, potentialNeighborCity, isConnected);
-  }
+        // This is an undirected graph. If we can go from city 0 to city 1,
+        // then we can go from city 1 to city 0.
+        // Thus, this graph CAN have cycles (all undirected graphs can have cycles),
+        // so we must tracked visited nodes to prevent infinite loops.
+        if (visited.has(potentialNeighborCity)) continue;
+
+        markProvinceAsVisited(visited, potentialNeighborCity, isConnected);
+    }
 };
 
 const findCircleNum = (isConnected) => {
-  const numCities = isConnected.length;
+    const numCities = isConnected.length;
 
-  let numProvinces = 0;
-  const visited = new Set();
+    let numProvinces = 0;
+    const visited = new Set();
 
-  for (let startCity = 0; startCity < numCities; startCity++) {
-    if (visited.has(startCity)) continue;
+    for (let startCity = 0; startCity < numCities; startCity++) {
+        if (visited.has(startCity)) continue;
 
-    numProvinces++;
-    markProvinceAsVisited(visited, startCity, isConnected);
-  }
+        numProvinces++;
+        markProvinceAsVisited(visited, startCity, isConnected);
+    }
 
-  return numProvinces;
+    return numProvinces;
 };
