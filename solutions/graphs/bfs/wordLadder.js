7bac16ae328c2b33816713486a6be3170291e0d2
shreshth <shreshthsrivastava2@gmail.com>
2022-07-10 19:55:36 -0400

    [solutions][bfs] Add visited set explanation for Word Ladder BFS JS.

--- solutions/graphs/bfs/wordLadder.js
+++ solutions/graphs/bfs/wordLadder.js
@@ -5,75 +5,80 @@ const getASCII = (char) => char.charCodeAt();
 const getChar = (asciiVal) => String.fromCharCode(asciiVal);
 
 const generateAllValidCharacterInsertions = (beginning, ending, validWords) => {
-  const allCharacterInsertions = [];
+    const allCharacterInsertions = [];
 
-  for (let num = getASCII('a'); num <= getASCII('z'); num += 1) {
-    const newChar = getChar(num);
-    const newString = beginning + newChar + ending;
+    for (let num = getASCII('a'); num <= getASCII('z'); num += 1) {
+        const newChar = getChar(num);
+        const newString = beginning + newChar + ending;
 
-    if (!validWords.has(newString)) continue;
+        if (!validWords.has(newString)) continue;
 
-    allCharacterInsertions.push(newString);
-  }
+        allCharacterInsertions.push(newString);
+    }
 
-  return allCharacterInsertions;
+    return allCharacterInsertions;
 };
 
 const generateValidOneDiffWords = (word, validWords) => {
-  const validOneDiffWords = [];
+    const validOneDiffWords = [];
 
-  for (let i = 0; i < word.length; i += 1) {
-    const beginning = word.substring(0, i);
-    const ending = word.substring(i + 1);
+    for (let i = 0; i < word.length; i += 1) {
+        const beginning = word.substring(0, i);
+        const ending = word.substring(i + 1);
 
-    const allCharacterInsertions = generateAllValidCharacterInsertions(
-      beginning,
-      ending,
-      validWords,
-    );
+        const allCharacterInsertions = generateAllValidCharacterInsertions(
+            beginning,
+            ending,
+            validWords,
+        );
 
-    // Add all of the `allCharacterInsertions` to `oneDiffWords`
-    allCharacterInsertions.forEach((newString) =>
-      validOneDiffWords.push(newString),
-    );
-  }
+        // Add all of the `allCharacterInsertions` to `oneDiffWords`
+        allCharacterInsertions.forEach((newString) =>
+            validOneDiffWords.push(newString),
+        );
+    }
 
-  return validOneDiffWords;
+    return validOneDiffWords;
 };
 
 const ladderLength = (beginWord, endWord, wordList) => {
-  const validWords = new Set(wordList);
-  if (!validWords.has(endWord)) return NO_VALID_SEQUENCE;
+    const validWords = new Set(wordList);
+    if (!validWords.has(endWord)) return NO_VALID_SEQUENCE;
 
-  const queue = new Queue();
-  queue.enqueue({
-    word: beginWord,
-    numTransformationsSoFar: 1,
-  });
+    const queue = new Queue();
+    queue.enqueue({
+        word: beginWord,
+        numTransformationsSoFar: 1,
+    });
 
-  const visited = new Set();
+    const visited = new Set();
 
-  while (queue.size() > 0) {
-    // Remove node
-    const { word, numTransformationsSoFar } = queue.dequeue();
+    while (queue.size() > 0) {
+        // Remove node
+        const { word, numTransformationsSoFar } = queue.dequeue();
 
-    // Process node
-    if (word === endWord) return numTransformationsSoFar;
+        // Process node
+        if (word === endWord) return numTransformationsSoFar;
 
-    visited.add(word);
+        visited.add(word);
 
-    // Add neighbors
-    const oneDiffWords = generateValidOneDiffWords(word, validWords);
+        // Add neighbors
+        const oneDiffWords = generateValidOneDiffWords(word, validWords);
 
-    for (const newWord of oneDiffWords) {
-      if (visited.has(newWord)) continue; // This graph CAN have cycles (all undirected graphs can have cycles), so we must tracked visited nodes to prevent infinite loops
+        for (const newWord of oneDiffWords) {
+            // This is an undirected graph. From the letter at position [1,3],
+            // we can go to the position [2,3] and from the position [2,3], we
+            // can go to the position [1,3].
+            // Thus, this graph CAN have cycles (all undirected graphs can have cycles),
+            // so we must tracked visited nodes to prevent infinite loops.
+            if (visited.has(newWord)) continue;
 
-      queue.enqueue({
-        word: newWord,
-        numTransformationsSoFar: numTransformationsSoFar + 1,
-      });
+            queue.enqueue({
+                word: newWord,
+                numTransformationsSoFar: numTransformationsSoFar + 1,
+            });
+        }
     }
-  }
 
-  return NO_VALID_SEQUENCE;
+    return NO_VALID_SEQUENCE;
 };
