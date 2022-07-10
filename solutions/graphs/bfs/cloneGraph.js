a2dff1e6d21d1a01966b1f0851b32313379bd627
shreshth <shreshthsrivastava2@gmail.com>
2022-07-10 19:44:31 -0400

    [solutions][bfs] Improve visited set explanation for Clone Graph BFS JS.

--- solutions/graphs/bfs/cloneGraph.js
+++ solutions/graphs/bfs/cloneGraph.js
@@ -1,42 +1,52 @@
 const cloneGraph = function (node) {
-  if (!node) {
-    return node;
-  }
+    if (!node) {
+        return node;
+    }
 
-  const queue = new Queue();
-  queue.enqueue(node);
+    const queue = new Queue();
+    queue.enqueue(node);
 
-  const originalToCloneMap = {};
-  originalToCloneMap[node.val] = new Node(node.val, []);
+    const originalToCloneMap = {};
+    originalToCloneMap[node.val] = new Node(node.val, []);
 
-  while (queue.size() > 0) {
-    // Remove from queue
-    const curNode = queue.dequeue();
+    while (queue.size() > 0) {
+        // Remove from queue
+        const curNode = queue.dequeue();
 
-    // Process node
-    const curNodeNeighbors = curNode.neighbors;
+        // Process node
+        const curNodeNeighbors = curNode.neighbors;
 
-    // Add neighbors
-    curNodeNeighbors.forEach((neighbor) => {
-      const hasNeighborClone = originalToCloneMap.hasOwnProperty(neighbor.val);
-      if (!hasNeighborClone) {
-        queue.enqueue(neighbor);
+        // Add neighbors
+        curNodeNeighbors.forEach((neighbor) => {
+            const hasNeighborClone = originalToCloneMap.hasOwnProperty(
+                neighbor.val,
+            );
 
-        // Note, for each node, we must create an entry in our originalToCloneMap
-        // BEFORE we process that node. This is so that other nodes are able to
-        // add its cloned version as a neighbor. For example, suppose we have [1,2].
-        // At node 1, we'll want to do something like node1Clone.neighbors.push(node2Clone).
-        // Thus, we must create node2Clone when processing node1, we cannot just wait until
-        // we process node2.
-        originalToCloneMap[neighbor.val] = new Node(neighbor.val, []);
-      }
+            // This is an undirected graph, so cycles are definitely possible. This means that
+            // we could traverse this graph forever, meaning we must keep track of which nodes
+            // we visit. This `!hasNeighborClone` check is implicitly checking if the current
+            // neighbor node has already been visited. This is because any time we visit a node,
+            // we create an entry for it in `originalToCloneMap`. Thus, if `originalToCloneMap`
+            // has no key for `neighbor.val` then we know that the current neighbor has NOT
+            // been visited yet. So, this functions as our visited check.
+            if (!hasNeighborClone) {
+                queue.enqueue(neighbor);
 
-      const curNodeClone = originalToCloneMap[curNode.val];
-      const neighborClone = originalToCloneMap[neighbor.val];
+                // Note, for each node, we must create an entry in our originalToCloneMap
+                // BEFORE we process that node. This is so that other nodes are able to
+                // add its cloned version as a neighbor. For example, suppose we have [1,2].
+                // At node 1, we'll want to do something like node1Clone.neighbors.push(node2Clone).
+                // Thus, we must create node2Clone when processing node1, we cannot just wait until
+                // we process node2.
+                originalToCloneMap[neighbor.val] = new Node(neighbor.val, []);
+            }
 
-      curNodeClone.neighbors.push(neighborClone);
-    });
-  }
+            const curNodeClone = originalToCloneMap[curNode.val];
+            const neighborClone = originalToCloneMap[neighbor.val];
 
-  return originalToCloneMap[node.val];
+            curNodeClone.neighbors.push(neighborClone);
+        });
+    }
+
+    return originalToCloneMap[node.val];
 };
