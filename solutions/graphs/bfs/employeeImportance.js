5a5cb6caa73a595d09ad65f89bbb21e22ed28d17
shreshth <shreshthsrivastava2@gmail.com>
2022-07-10 19:39:54 -0400

    [solutions][bfs] Improve visited set explanation for Employee Importance BFS JS.

--- solutions/graphs/bfs/employeeImportance.js
+++ solutions/graphs/bfs/employeeImportance.js
@@ -1,42 +1,48 @@
 const buildEmployeeMap = (employees) => {
-  const idToEmployeeMap = {};
+    const idToEmployeeMap = {};
 
-  for (let i = 0; i < employees.length; i++) {
-    const curEmployee = employees[i];
-    idToEmployeeMap[curEmployee.id] = curEmployee;
-  }
+    for (let i = 0; i < employees.length; i++) {
+        const curEmployee = employees[i];
+        idToEmployeeMap[curEmployee.id] = curEmployee;
+    }
 
-  return idToEmployeeMap;
+    return idToEmployeeMap;
 };
 
 const getTotalImportance = (rootId, idToEmployeeMap) => {
-  let totalImportance = 0;
+    let totalImportance = 0;
 
-  const queue = new Queue();
-  queue.enqueue(rootId);
+    const queue = new Queue();
+    queue.enqueue(rootId);
 
-  while (queue.size() > 0) {
-    // Remove node
-    const employeeId = queue.dequeue();
+    while (queue.size() > 0) {
+        // Remove node
+        const employeeId = queue.dequeue();
 
-    // Process node
-    const employee = idToEmployeeMap[employeeId];
-    const { id, importance, subordinates } = employee;
+        // Process node
+        const employee = idToEmployeeMap[employeeId];
+        const { id, importance, subordinates } = employee;
 
-    totalImportance += importance;
+        totalImportance += importance;
 
-    // Add neighbors
-    subordinates.forEach((subordinate) => {
-      // This is a DAG (directed acylclic graph) meaning we do not need any visited checks
-      queue.enqueue(subordinate);
-    });
-  }
+        // Add neighbors
+        subordinates.forEach((subordinate) => {
+            // This is a DAG (directed acyclic graph) meaning we do NOT need any visited checks.
+            // What would a cycle mean in this context?
+            // Imagine that Bob is your manager, and Abby is Bob's manager, but you are also
+            // Abby's manager. Clearly, that does not make sense (because you would be in
+            // charge of your own grandmanager). So, we see that in this employee hierarchy
+            // graph specified for this problem, cycles are not possible. Thus, we do NOT
+            // need any visited checks.
+            queue.enqueue(subordinate);
+        });
+    }
 
-  return totalImportance;
+    return totalImportance;
 };
 
 const GetImportance = function (employees, id) {
-  const idToEmployeeMap = buildEmployeeMap(employees);
+    const idToEmployeeMap = buildEmployeeMap(employees);
 
-  return getTotalImportance(id, idToEmployeeMap);
+    return getTotalImportance(id, idToEmployeeMap);
 };
