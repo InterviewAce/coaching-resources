a8ad0d615063355b9d4fa7c1bc975648f61f36f6
shreshth <shreshthsrivastava2@gmail.com>
2022-07-10 20:00:51 -0400

    [solutions][dfs] Add visited set explanation for Keys and Rooms DFS JS.

--- solutions/graphs/dfs/keysAndRooms.js
+++ solutions/graphs/dfs/keysAndRooms.js
@@ -1,29 +1,32 @@
 const ROOM_ZERO = 0;
 
 const canVisitAllRooms = (rooms) => {
-  const stack = [];
-  stack.push(ROOM_ZERO);
+    const stack = [];
+    stack.push(ROOM_ZERO);
 
-  const visitedRooms = new Set();
+    const visitedRooms = new Set();
 
-  while (stack.length > 0) {
-    // Remove node
-    const roomNumber = stack.pop();
+    while (stack.length > 0) {
+        // Remove node
+        const roomNumber = stack.pop();
 
-    // Process node
-    if (visitedRooms.has(roomNumber)) continue;
+        // Process node
+        // This graph is directed, but it still CAN have cycles. For example, room 0
+        // can have the key to room 4, and room 4 can have the key to room 0.
+        // So we must tracked visited nodes to prevent infinite loops.
+        if (visitedRooms.has(roomNumber)) continue;
 
-    visitedRooms.add(roomNumber);
-    const unlockableRooms = rooms[roomNumber];
+        visitedRooms.add(roomNumber);
+        const unlockableRooms = rooms[roomNumber];
 
-    // Add neighbors
-    for (const unlockableRoom of unlockableRooms) {
-      stack.push(unlockableRoom);
+        // Add neighbors
+        for (const unlockableRoom of unlockableRooms) {
+            stack.push(unlockableRoom);
+        }
     }
-  }
 
-  const numRoomsCanVisit = visitedRooms.size;
-  const numRooms = rooms.length;
+    const numRoomsCanVisit = visitedRooms.size;
+    const numRooms = rooms.length;
 
-  return numRoomsCanVisit == numRooms;
+    return numRoomsCanVisit == numRooms;
 };
