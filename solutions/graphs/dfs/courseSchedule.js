0daa0d009642b2b34b6707b232417c2f6ddff4a5
shreshth <shreshthsrivastava2@gmail.com>
2022-07-11 08:24:56 -0700

    [solutions][dfs] Update Course Schedule JS solution to remove magic numbers.

--- solutions/graphs/dfs/courseSchedule.js
+++ solutions/graphs/dfs/courseSchedule.js
@@ -1,46 +1,53 @@
-const buildGraph = (prerequisites) => {
+const CURRENTLY_VISITING = -1;
+const UNVISITED = 0;
+const PREVIOUSLY_VISITED = 1;
+
+const buildGraph = (dependencies) => {
+    // Note that we have made an effort to keep this function generic & reusable
     const graph = {};
 
-    prerequisites.forEach(([course, prereq]) => {
+    dependencies.forEach(([node, prereq]) => {
         const prereqInGraph = graph.hasOwnProperty(prereq);
 
         if (!prereqInGraph) graph[prereq] = [];
 
-        graph[prereq].push(course);
+        graph[prereq].push(node);
     });
 
     return graph;
 };
 
-const createArrayOfSize = (size, fillValue) => {
-    return new Array(size).fill(fillValue);
-};
+const checkIfHasCycle = (courseNum, graph, visited) => {
+    // Base case
+    if (visited[courseNum] == CURRENTLY_VISITING) return true;
 
-const checkForCycle = (courseNum, graph, visited) => {
-    if (visited[courseNum] == -1) return true;
-    visited[courseNum] = -1;
+    // Process node
+    visited[courseNum] = CURRENTLY_VISITING;
 
-    const neighbours = graph[courseNum] || [];
+    // Recurse on neighbors
 
-    for (const neighbor of neighbours) {
+    // If graph[courseNum] is undefined, set neighbors to be []
+    const neighbors = graph[courseNum] || [];
+
+    for (const neighbor of neighbors) {
         // This graph is directed, but it still CAN have cycles. For example, course 0
         // can require course 4, and course 4 can require course 0.
         // So, we must tracked visited nodes to prevent infinite loops.
-        if (visited[n] != 1) {
-            if (checkForCycle(neighbor, graph, visited)) return true;
+        if (visited[neighbor] != PREVIOUSLY_VISITED) {
+            if (checkIfHasCycle(neighbor, graph, visited)) return true;
         }
     }
 
-    visited[courseNum] = 1;
+    visited[courseNum] = PREVIOUSLY_VISITED;
     return false;
 };
 
 const canFinish = (numCourses, prerequisites) => {
     const graph = buildGraph(prerequisites);
-    const visited = createArrayOfSize(numCourses, 0); // 0 means unvisited
+    const visited = new Array(numCourses).fill(UNVISITED);
 
     for (let courseNum = 0; courseNum < numCourses; courseNum++) {
-        const hasCycle = checkForCycle(courseNum, graph, visited);
+        const hasCycle = checkIfHasCycle(courseNum, graph, visited);
 
         if (hasCycle) return false;
     }
