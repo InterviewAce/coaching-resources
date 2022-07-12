4f34a749fdbb9f92046aeaf26110d3f75355a6ae
shreshth <shreshthsrivastava2@gmail.com>
2022-07-11 21:00:13 -0700

    [py][bfs] Add Number of Connected Components in an Undirected Graph solution.

---
+++ solutions/python/graphs/bfs/number_of_connected_components_in_an_undirected_graph.py
@@ -0,0 +1,44 @@
+class Solution:
+    def build_graph(self, edges):
+        graph = defaultdict(list)
+        
+        for node_one, node_two in edges:
+            graph[node_one].append(node_two)
+            graph[node_two].append(node_one)
+        
+        return graph
+    
+    def mark_component_as_visited(self, graph, start_node, visited):
+        queue = deque()
+        queue.append(start_node)
+        
+        while queue:
+            # Remove node
+            node = queue.popleft()
+            
+            # Process node
+            visited.add(node)
+            
+            # Add neighbors
+            neighbors = graph[node]
+            
+            for neighbor in neighbors:
+                if neighbor in visited:
+                    continue
+                    
+                queue.append(neighbor)
+    
+    def countComponents(self, num_nodes: int, edges: List[List[int]]) -> int:
+        graph = self.build_graph(edges)
+        
+        num_connected_components = 0
+        visited = set()
+        
+        for node in range(num_nodes):
+            if node in visited:
+                continue
+                
+            num_connected_components += 1
+            self.mark_component_as_visited(graph, node, visited)
+            
+        return num_connected_components
