44a25484d65515210e5724f3fb19af45397eb89c
shreshth <shreshthsrivastava2@gmail.com>
2022-07-14 16:52:13 -0700

    [js][class-design] Clean up Implement Trie solution.

--- solutions/javascript/class-design/implementTrie.js
+++ solutions/javascript/class-design/implementTrie.js
@@ -1,45 +1,47 @@
-var Trie = function () {
-  this.trie = {};
-  this.endSymbol = '*';
-};
+class Trie {
+    constructor() {
+        this.trie = {};
+        this.endSymbol = '*';
+    }
 
-Trie.prototype.insert = function (word) {
-  let curPosition = this.trie;
+    insert(word) {
+        let curPosition = this.trie;
 
-  for (const char of word) {
-    const charInCurPosition = curPosition.hasOwnProperty(char);
+        for (const char of word) {
+            const charInCurPosition = curPosition.hasOwnProperty(char);
 
-    if (!charInCurPosition) curPosition[char] = {};
-    curPosition = curPosition[char];
-  }
+            if (!charInCurPosition) curPosition[char] = {};
+            curPosition = curPosition[char];
+        }
 
-  curPosition[this.endSymbol] = true;
-};
+        curPosition[this.endSymbol] = true;
+    }
 
-Trie.prototype.search = function (word) {
-  let curPosition = this.trie;
+    search(word) {
+        let curPosition = this.trie;
 
-  for (const char of word) {
-    const charInCurPosition = curPosition.hasOwnProperty(char);
+        for (const char of word) {
+            const charInCurPosition = curPosition.hasOwnProperty(char);
 
-    if (!charInCurPosition) return false;
-    curPosition = curPosition[char];
-  }
+            if (!charInCurPosition) return false;
+            curPosition = curPosition[char];
+        }
 
-  const endSymbolExists = curPosition.hasOwnProperty(this.endSymbol);
+        const endSymbolExists = curPosition.hasOwnProperty(this.endSymbol);
 
-  return endSymbolExists;
-};
+        return endSymbolExists;
+    }
 
-Trie.prototype.startsWith = function (prefix) {
-  let curPosition = this.trie;
+    startsWith(prefix) {
+        let curPosition = this.trie;
 
-  for (const char of prefix) {
-    const charInCurPosition = curPosition.hasOwnProperty(char);
+        for (const char of prefix) {
+            const charInCurPosition = curPosition.hasOwnProperty(char);
 
-    if (!charInCurPosition) return false;
-    curPosition = curPosition[char];
-  }
+            if (!charInCurPosition) return false;
+            curPosition = curPosition[char];
+        }
 
-  return true;
-};
+        return true;
+    }
+}
