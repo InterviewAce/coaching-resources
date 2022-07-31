5ffc10baebbb1f3187a2c30275dc41cd71396758
shreshth <shreshthsrivastava2@gmail.com>
2022-07-31 15:33:57 -0700

    [js][dfs] Add Is Graph Bipartite? solution.

---
+++ solutions/javascript/dfs/isGraphBipartite.js
@@ -0,0 +1,51 @@
+const SET_A = 1;
+const SET_B = -1;
+const UNVISITED = 0;
+
+const checkIfCanBeBipartite = (graph, node, visited, prevColor) => {
+    // Base cases
+    if (visited[node] !== UNVISITED) return visited[node] !== prevColor;
+
+    // Process node
+    if (prevColor === null || prevColor === SET_B) visited[node] = SET_A;
+    else visited[node] = SET_B;
+
+    // Handle neighbors
+    const neighbors = graph[node];
+    for (const neighbor of neighbors) {
+        const canBeBipartite = checkIfCanBeBipartite(
+            graph,
+            neighbor,
+            visited,
+            visited[node],
+        );
+
+        if (!canBeBipartite) return false;
+    }
+
+    return true;
+};
+
+const isBipartite = (graph) => {
+    const numNodes = graph.length;
+    const visited = new Array(numNodes).fill(UNVISITED);
+
+    for (let node = 0; node < numNodes; node++) {
+        visited[node] = UNVISITED;
+    }
+
+    for (let node = 0; node < numNodes; node++) {
+        if (visited[node] !== UNVISITED) continue;
+
+        const canBeBipartite = checkIfCanBeBipartite(
+            graph,
+            node,
+            visited,
+            null,
+        );
+
+        if (!canBeBipartite) return false;
+    }
+
+    return true;
+};
