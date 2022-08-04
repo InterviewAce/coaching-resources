8909813079910cb81c0ed812b9c09864532898a7
shreshth <shreshthsrivastava2@gmail.com>
2022-08-03 19:25:54 -0700

    [js][bfs] Add Shortest Path in Binary Matrix solution.

--- solutions/javascript/bfs/shortestPathInBinaryMatrix.js
+++ solutions/javascript/bfs/shortestPathInBinaryMatrix.js
@@ -1,94 +1,115 @@
-/*
-What is our implicit graph?
--What are our nodes? Each cell in our matrix/grid is a node (e.g. grid[row][col]).
--What are our edges/neighbors? The neighbors are the (at most) 8 cells that are adjacent
-to the current cell (meaning they are touching horizontally, vertically, or diagonally).
-
-A node would look like: (0,0)
-An edge would look like: [(0,0), (1,1)]
-*/
-
-/*
-This graph is undirected (e.g. (0,0) is connected to (1,1) AND (1,1) is connected to (0,0)).
-This means we must use a visited set to prevent cycles.
-*/
-const CLEAR = 0;
-const directions = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]];
+const TOP_LEFT_CELL_ROW = 0;
+const TOP_LEFT_CELL_COL = 0;
 const NO_CLEAR_PATH = -1;
+const CLEAR = 0;
 
-class Node {
-    constructor(row, col, curPathSize) {
-        this.row = row;
-        this.col = col;
-        this.curPathSize = curPathSize;
-    }
-}
+const directions = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+];
 
-const getNeighbors = (row, col) => {
-    const neighbors = [];
-    
+const getPositionString = (row, col) => `${row}, ${col}`;
+
+const getAdjacentPositions = (row, col) => {
+    const adjacentPositions = [];
+
     for (const direction of directions) {
         const [rowChange, colChange] = direction;
-        
+
         const newRow = row + rowChange;
         const newCol = col + colChange;
-        
-        neighbors.push([newRow, newCol]);
+
+        adjacentPositions.push([newRow, newCol]);
     }
-    
-    return neighbors;
-}
+
+    return adjacentPositions;
+};
 
 const isInBounds = (grid, row, col) => {
     const numRows = grid.length;
     const numCols = grid[0].length;
-    
+
     const rowInBounds = row >= 0 && row < numRows;
     const colInBounds = col >= 0 && col < numCols;
-    
+
     return rowInBounds && colInBounds;
-}
+};
+
+const getNeighbors = (grid, row, col) => {
+    const adjacentPositions = getAdjacentPositions(row, col); // [[0,0], [0,1], [-1,0], ...]
+    const neighbors = [];
+
+    for (const adjacentPosition of adjacentPositions) {
+        const [adjacentPositionRow, adjacentPositionCol] = adjacentPosition;
+
+        if (!isInBounds(grid, adjacentPositionRow, adjacentPositionCol))
+            continue;
+        if (grid[adjacentPositionRow][adjacentPositionCol] !== CLEAR) continue;
+
+        // We know the adjacent position is in bounds AND is a 0. Therefore, it is
+        // a neighbor.
+        neighbors.push([adjacentPositionRow, adjacentPositionCol]);
+    }
+
+    return neighbors;
+};
 
 const shortestPathBinaryMatrix = (grid) => {
+    if (grid[TOP_LEFT_CELL_ROW][TOP_LEFT_CELL_COL] !== CLEAR)
+        return NO_CLEAR_PATH;
+
     const numRows = grid.length;
     const numCols = grid[0].length;
-    
-    if (grid[0][0] !== CLEAR || grid[numRows - 1][numCols - 1] !== CLEAR) return NO_CLEAR_PATH;
-    
-    const startNode = new Node(0, 0, 1);
-    const visited = new Set();
-    
+
     const queue = new Queue();
-    queue.enqueue(startNode);
-    
-    while (queue.size() > 0) {
+    const visited = new Set();
+
+    queue.enqueue({
+        row: TOP_LEFT_CELL_ROW,
+        col: TOP_LEFT_CELL_COL,
+        pathLengthSoFar: 1,
+    });
+
+    const startPositionString = getPositionString(
+        TOP_LEFT_CELL_ROW,
+        TOP_LEFT_CELL_COL,
+    );
+    visited.add(startPositionString);
+
+    while (!queue.isEmpty()) {
         // Remove node
-        const { row, col, curPathSize } = queue.dequeue();
-        
+        const { row, col, pathLengthSoFar } = queue.dequeue();
+
         // Process node
-        const curPosition = `${row}, ${col}`;
-        
-        // We've tried adding the visited check in the "add neighbors" step, but it doesn't work for this problem. We're not totally sure why.
-        if (visited.has(curPosition)) continue;
-        
-        visited.add(curPosition);
-        
-        const isEndPosition = row === numRows - 1 && col === numCols - 1;
-        if (isEndPosition) return curPathSize;
-        
+        // We know that the current cell is in bounds and is a 0
+        const isBottomRightCell = row === numRows - 1 && col === numCols - 1;
+        if (isBottomRightCell) return pathLengthSoFar;
+
         // Add neighbors
-        const neighbors = getNeighbors(row, col);
+        const neighbors = getNeighbors(grid, row, col); // [[0,0], [0,1], [1,0], ...]
         for (const neighbor of neighbors) {
             const [neighborRow, neighborCol] = neighbor;
-            const neighborPosition = `${neighborRow}, ${neighborCol}`;
-            
-            if (!isInBounds(grid, neighborRow, neighborCol)) continue;
-            if (grid[neighborRow][neighborCol] !== CLEAR) continue;
-            
-            const neighborNode = new Node(neighborRow, neighborCol, curPathSize + 1);
-            queue.enqueue(neighborNode);
+            const neighborPositionString = getPositionString(
+                neighborRow,
+                neighborCol,
+            );
+
+            if (visited.has(neighborPositionString)) continue;
+            visited.add(neighborPositionString);
+
+            queue.enqueue({
+                row: neighborRow,
+                col: neighborCol,
+                pathLengthSoFar: pathLengthSoFar + 1,
+            });
         }
     }
-    
+
     return NO_CLEAR_PATH;
 };
