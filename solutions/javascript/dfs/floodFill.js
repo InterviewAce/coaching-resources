e8636418ff3953117c5b7f5a0c5f9e96be05936d
shreshth <shreshthsrivastava2@gmail.com>
2022-08-09 21:59:04 -0700

    [js][dfs] Add Flood Fill solution.

--- solutions/javascript/dfs/floodFill.js
+++ solutions/javascript/dfs/floodFill.js
@@ -5,7 +5,9 @@ const directions = [
     [-1, 0],
 ];
 
-const isValidPosition = (grid, row, col) => {
+const getPositionString = (row, col) => `${row}, ${col}`;
+
+const isInBounds = (grid, row, col) => {
     const numRows = grid.length;
     const numCols = grid[0].length;
 
@@ -15,28 +17,57 @@ const isValidPosition = (grid, row, col) => {
     return rowInBounds && colInBounds;
 };
 
-const replaceColorOfConnectedComponent = (row, col, initialPixelColor, newPixelColor, image) => {
-    // Base case
-    if (!isValidPosition(image, row, col)) return;
-    if (image[row][col] !==  initialPixelColor) return;
+const replaceColorOfConnectedComponent = (
+    image,
+    row,
+    col,
+    colorToReplace,
+    newColor,
+    visited,
+) => {
+    // Base cases
+    if (!isInBounds(image, row, col)) return;
+
+    const curPositionString = getPositionString(row, col);
+    if (visited.has(curPositionString)) return;
+
+    const curColor = image[row][col];
+    if (curColor !== colorToReplace) return;
 
     // Process node
-    image[row][col] = newPixelColor;
+    image[row][col] = newColor;
+    visited.add(curPositionString);
 
-    // Recurse on neighbors
-    // Try going up, left, down, and right
+    // Recurse on potential neighbors
     for (const direction of directions) {
         const [rowChange, colChange] = direction;
 
         const newRow = row + rowChange;
         const newCol = col + colChange;
 
-        replaceColorOfConnectedComponent(newRow, newCol, initialPixelColor, newPixelColor, image);
+        replaceColorOfConnectedComponent(
+            image,
+            newRow,
+            newCol,
+            colorToReplace,
+            newColor,
+            visited,
+        );
     }
 };
 
-const floodFill = (row, col, newPixelColor, image) => {
-    const initialPixelColor = image[row][col];
-    replaceColorOfConnectedComponent(row, col, initialPixelColor, newPixelColor, image);
+const floodFill = (row, col, newColor, image) => {
+    const colorToReplace = image[row][col];
+    const visited = new Set();
+
+    replaceColorOfConnectedComponent(
+        image,
+        row,
+        col,
+        colorToReplace,
+        newColor,
+        visited,
+    );
+
     return image;
 };
