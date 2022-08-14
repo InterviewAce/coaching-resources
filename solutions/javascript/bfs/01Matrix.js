e106935e64b21c6fbf9aab664ae426d20cbff56b
shreshth <shreshthsrivastava2@gmail.com>
2022-08-13 20:40:44 -0700

    [js][bfs] Add multi-sourced BFS solution for 01 Matrix.

--- solutions/javascript/bfs/01Matrix.js
+++ solutions/javascript/bfs/01Matrix.js
@@ -133,3 +133,119 @@ const updateMatrix = (matrix) => {
 
     return distanceToNearestZeroGrid;
 };
+
+/* Multi-sourced BFS solution, passes on LeetCode */
+const directions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+];
+
+const getPositionString = (row, col) => `${row}, ${col}`;
+
+const createArrayOfSize = (numRows, numCols, fillValue = undefined) => {
+    const array = new Array(numRows);
+
+    for (let row = 0; row < numRows; row += 1) {
+        array[row] = new Array(numCols).fill(fillValue);
+    }
+
+    return array;
+};
+
+const getCellsWithZero = (matrix, numRows, numCols) => {
+    const cellsWithZero = [];
+
+    for (let row = 0; row < numRows; row += 1) {
+        for (let col = 0; col < numCols; col += 1) {
+            const cell = matrix[row][col];
+
+            if (cell === 0) cellsWithZero.push([row, col]);
+        }
+    }
+
+    return cellsWithZero;
+};
+
+const isInBounds = (matrix, row, col) => {
+    const numRows = matrix.length;
+    const numCols = matrix[0].length;
+
+    const rowInBounds = row >= 0 && row < numRows;
+    const colInBounds = col >= 0 && col < numCols;
+
+    return rowInBounds && colInBounds;
+};
+
+const getNeighbors = (matrix, row, col) => {
+    const neighbors = [];
+
+    for (const direction of directions) {
+        const [rowChange, colChange] = direction;
+
+        const newRow = row + rowChange;
+        const newCol = col + colChange;
+
+        if (!isInBounds(matrix, newRow, newCol)) continue;
+
+        neighbors.push([newRow, newCol]);
+    }
+
+    return neighbors;
+};
+
+const updateMatrix = (matrix) => {
+    const numRows = matrix.length;
+    const numCols = matrix[0].length;
+
+    const distanceToNearestZero = createArrayOfSize(numRows, numCols, Infinity);
+
+    const cellsWithZero = getCellsWithZero(matrix, numRows, numCols);
+
+    const queue = new Queue();
+    const visited = new Set();
+
+    for (cell of cellsWithZero) {
+        const [row, col] = cell;
+        queue.enqueue({
+            row,
+            col,
+            distanceSoFar: 0,
+        });
+
+        const cellString = getPositionString(row, col);
+        visited.add(cellString);
+    }
+
+    while (!queue.isEmpty()) {
+        // Remove node
+        const { row, col, distanceSoFar } = queue.dequeue();
+
+        // Process node
+        if (matrix[row][col] === 0) distanceToNearestZero[row][col] = 0;
+        if (matrix[row][col] === 1)
+            distanceToNearestZero[row][col] = distanceSoFar;
+
+        // Add neighbors
+        const neighbors = getNeighbors(matrix, row, col);
+        for (const neighbor of neighbors) {
+            const [neighborRow, neighborCol] = neighbor;
+            const neighborPositionString = getPositionString(
+                neighborRow,
+                neighborCol,
+            );
+
+            if (visited.has(neighborPositionString)) continue;
+            visited.add(neighborPositionString);
+
+            queue.enqueue({
+                row: neighborRow,
+                col: neighborCol,
+                distanceSoFar: distanceSoFar + 1,
+            });
+        }
+    }
+
+    return distanceToNearestZero;
+};
