92426e987fe32efdc857fa944f83c28aa97918d2
Rohin Manvi <37752545+rohinmanvi@users.noreply.github.com>
2022-08-23 20:42:27 -0700

    update mergeTwoSortedLists

--- solutions/javascript/blind-75/linked-list/mergeTwoSortedLists.js
+++ solutions/javascript/blind-75/linked-list/mergeTwoSortedLists.js
@@ -1,30 +1,20 @@
-const mergeTwoLists = (list1, list2) => {
-    // We start by creating a dummy node so that we can add new nodes without checking for null
+const mergeTwoLists = (listOne, listTwo) => {
     const sentinel = new ListNode();
+    let previous = sentinel;
 
-    let endOfMergedList = sentinel;
-
-    const listOneNode = list1;
-    const listTwoNode = list2;
-
-    while (listOneNode && listTwoNode) {
-        if (listOneNode.val < listTwoNode.val) {
-            endOfMergedList.next = listOneNode;
-            listOneNode = listOneNode.next;
+    while (listOne && listTwo) {
+        if (listOne.val < listTwo.val) {
+            previous.next = listOne;
+            listOne = listOne.next;
         } else {
-            endOfMergedList.next = listTwoNode;
-            listTwoNode = listTwoNode.next;
+            previous.next = listTwo;
+            listTwo = listTwo.next;
         }
-
-        endOfMergedList = endOfMergedList.next;
+        previous = previous.next;
     }
 
-    if (listOneNode) {
-        endOfMergedList.next = listOneNode;
-    } else {
-        endOfMergedList.next = listTwoNode;
-    }
+    if (listOne) previous.next = listOne;
+    if (listTwo) previous.next = listTwo;
 
-    // We skip the dummy node to return the beginning of the merged list
     return sentinel.next;
 };
