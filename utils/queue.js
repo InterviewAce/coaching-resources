38be3213186aedcb31e68d874eec514fb260d1f1
shreshth <shreshthsrivastava2@gmail.com>
2022-08-26 22:54:32 -0700

    [js][utils] Add Queue class.

---
+++ utils/queue.js
@@ -0,0 +1,30 @@
+class Queue {
+    constructor(array = []) {
+        this.elements = array;
+
+        this.startIdx = 0;
+        this.endIdx = array.length - 1;
+    }
+
+    enqueue(newElement) {
+        this.elements.push(newElement);
+        this.endIdx++;
+    }
+
+    dequeue() {
+        if (this.isEmpty()) return null;
+
+        const returnElement = this.elements[this.startIdx];
+
+        this.startIdx++;
+        return returnElement;
+    }
+
+    isEmpty() {
+        return this.size() <= 0;
+    }
+
+    size() {
+        return this.endIdx - this.startIdx + 1;
+    }
+}
