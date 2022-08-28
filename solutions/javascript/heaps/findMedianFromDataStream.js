c20e9d425982a03500cf2f6835b781aa122fc919
Rohin Manvi <37752545+rohinmanvi@users.noreply.github.com>
2022-08-28 13:32:39 -0700

    update findMedianFromDataStream

--- solutions/javascript/heaps/findMedianFromDataStream.js
+++ solutions/javascript/heaps/findMedianFromDataStream.js
@@ -124,14 +124,121 @@ class MinHeap {
     }
 }
 
+class MaxHeap {
+    constructor(elements = [], comparisonFunction = null) {
+        this.elements = elements;
+
+        this.comparisonFunction =
+            comparisonFunction !== null ? comparisonFunction : (a, b) => a - b;
+
+        this.FIRST_IDX = 0;
+
+        for (let i = this.FIRST_IDX; i <= this.elements.length; i += 1) {
+            this.siftDown(i);
+        }
+    }
+
+    push(value) {
+        this.elements.push(value);
+
+        const lastIdx = this.size() - 1;
+        this.siftUp(lastIdx);
+    }
+
+    peek() {
+        if (this.size() === 0) return null;
+        return this.elements[this.FIRST_IDX];
+    }
+
+    swapElements(i, j) {
+        const temp = this.elements[i];
+        this.elements[i] = this.elements[j];
+        this.elements[j] = temp;
+    }
+
+    pop() {
+        if (this.size() === 0) return null;
+
+        const lastIdx = this.size() - 1;
+        this.swapElements(this.FIRST_IDX, lastIdx);
+
+        const elementToReturn = this.elements.pop();
+        this.siftDown(this.FIRST_IDX);
+
+        return elementToReturn;
+    }
+
+    size() {
+        return this.elements.length;
+    }
+
+    getParentIdx(curIdx) {
+        return Math.floor((curIdx - 1) / 2);
+    }
+
+    getLeftChildIdx(parentIdx) {
+        return parentIdx * 2 + 1;
+    }
+
+    getRightChildIdx(parentIdx) {
+        return parentIdx * 2 + 2;
+    }
+
+    siftUp(curIdx) {
+        while (curIdx > this.FIRST_IDX) {
+            const parentIdx = this.getParentIdx(curIdx);
+
+            const curElement = this.elements[curIdx];
+            const parentElement = this.elements[parentIdx];
+
+            if (this.comparisonFunction(curElement, parentElement) > 0) {
+                this.swapElements(curIdx, parentIdx);
+                curIdx = parentIdx;
+            } else break;
+        }
+    }
+
+    getMaxChildIdx(curIdx) {
+        // We assume that at least one child exists
+
+        const leftChildIdx = this.getLeftChildIdx(curIdx);
+        const rightChildIdx = this.getRightChildIdx(curIdx);
+
+        if (rightChildIdx >= this.size()) return leftChildIdx;
+
+        const leftChild = this.elements[leftChildIdx];
+        const rightChild = this.elements[rightChildIdx];
+
+        if (this.comparisonFunction(leftChild, rightChild) > 0) return leftChildIdx;
+        return rightChildIdx;
+    }
+
+    siftDown(curIdx) {
+        while (this.getLeftChildIdx(curIdx) < this.size()) {
+            // console.log(curIdx);
+            const maxChildIdx = this.getMaxChildIdx(curIdx);
+
+            const curElement = this.elements[curIdx];
+            const maxChildElement = this.elements[maxChildIdx];
+
+            if (this.comparisonFunction(maxChildElement, curElement) > 0)
+                this.swapElements(curIdx, maxChildIdx);
+            else break;
+
+            curIdx = maxChildIdx;
+        }
+    }
+
+    // Use this for debugging purposes
+    getElements() {
+        return this.elements;
+    }
+}
+
 class MedianFinder {
     constructor() {
+        this.lowerHalf = new MaxHeap();
         this.higherHalf = new MinHeap();
-        
-        const reverseComparison = (a, b) => b - a;
-        const maxHeap = new MinHeap([], reverseComparison);
-        
-        this.lowerHalf = maxHeap;
     }
     
     balanceHalves(halfOne, halfTwo) {
