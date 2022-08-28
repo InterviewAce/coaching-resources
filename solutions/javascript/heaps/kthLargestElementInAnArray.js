c586b4061e2e28ebaed923a3f13ff42bbcf92b24
shreshth <shreshthsrivastava2@gmail.com>
2022-08-27 20:10:04 -0700

    [js][heaps] Fix Kth Largest Element in an Array solution.

--- solutions/javascript/heaps/kthLargestElementInAnArray.js
+++ solutions/javascript/heaps/kthLargestElementInAnArray.js
@@ -1,16 +1,131 @@
-// https://www.npmjs.com/package/heap-js
-const heapJS = require('heap-js');
+/*
+When passing a comparisonFunction, your comparisonFunction should take in
+two elements a, b and return a positive number if a is greater than b,
+a negative number if a is less than b, and 0 if a is equal to b.
 
-const findKthLargest = (nums, k) => {
-    const kLargest = new heapJS.Heap();
-    
-    for (const num of nums) {
-        kLargest.push(num);
-        if (kLargest.size() > k) {
-            kLargest.pop();
+For example, (a, b) => a - b works if we are just dealing with two numbers.
+*/
+
+class MinHeap {
+    constructor(elements = [], comparisonFunction = null) {
+        this.elements = elements;
+
+        this.comparisonFunction =
+            comparisonFunction !== null ? comparisonFunction : (a, b) => a - b;
+
+        this.FIRST_IDX = 0;
+
+        for (let i = this.FIRST_IDX; i < this.elements.length; i += 1) {
+            this.siftDown(i);
         }
     }
-    
-    const kthLargest = kLargest.pop();
-    return kthLargest;
+
+    push(value) {
+        this.elements.push(value);
+
+        const lastIdx = this.size() - 1;
+        this.siftUp(lastIdx);
+    }
+
+    peek() {
+        if (this.size() === 0) return null;
+        return this.elements[this.FIRST_IDX];
+    }
+
+    swapElements(i, j) {
+        const temp = this.elements[i];
+        this.elements[i] = this.elements[j];
+        this.elements[j] = temp;
+    }
+
+    pop() {
+        if (this.size() === 0) return null;
+
+        const lastIdx = this.size() - 1;
+        this.swapElements(this.FIRST_IDX, lastIdx);
+
+        const elementToReturn = this.elements.pop();
+        this.siftDown(this.FIRST_IDX);
+
+        return elementToReturn;
+    }
+
+    size() {
+        return this.elements.length;
+    }
+
+    getParentIdx(curIdx) {
+        return Math.floor((curIdx - 1) / 2);
+    }
+
+    getLeftChildIdx(parentIdx) {
+        // TODO: change?
+        return parentIdx * 2 + 1;
+    }
+
+    getRightChildIdx(parentIdx) {
+        return parentIdx * 2 + 2;
+    }
+
+    siftUp(curIdx) {
+        while (curIdx > this.FIRST_IDX) {
+            const parentIdx = this.getParentIdx(curIdx);
+
+            const curElement = this.elements[curIdx];
+            const parentElement = this.elements[parentIdx];
+
+            if (this.comparisonFunction(curElement, parentElement) < 0) {
+                this.swapElements(curIdx, parentIdx);
+                curIdx = parentIdx;
+            } else break;
+        }
+    }
+
+    getMinChildIdx(curIdx) {
+        // We assume that at least one child exists
+
+        const leftChildIdx = this.getLeftChildIdx(curIdx);
+        const rightChildIdx = this.getRightChildIdx(curIdx);
+
+        if (rightChildIdx >= this.size()) return leftChildIdx;
+
+        const leftChild = this.elements[leftChildIdx];
+        const rightChild = this.elements[rightChildIdx];
+
+        if (this.comparisonFunction(leftChild, rightChild) < 0) return leftChildIdx;
+        return rightChildIdx;
+    }
+
+    siftDown(curIdx) {
+        while (this.getLeftChildIdx(curIdx) < this.size()) {
+            const minChildIdx = this.getMinChildIdx(curIdx);
+
+            const curElement = this.elements[curIdx];
+            const minChildElement = this.elements[minChildIdx];
+
+            if (this.comparisonFunction(minChildElement, curElement) < 0)
+                this.swapElements(curIdx, minChildIdx);
+            else break;
+
+            curIdx = minChildIdx;
+        }
+    }
+
+    // Use this for debugging purposes
+    getElements() {
+        return this.elements;
+    }
+}
+
+const findKthLargest = (nums, k) => {
+    const kthLargestElements = new MinHeap();
+
+    for (const num of nums) {
+        kthLargestElements.push(num);
+
+        if (kthLargestElements.size() > k) kthLargestElements.pop();
+    }
+
+    const kthLargestElement = kthLargestElements.pop();
+    return kthLargestElement;
 };
