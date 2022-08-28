007b8c754d27afc293b90bed306a18314b9c4eb5
shreshth <shreshthsrivastava2@gmail.com>
2022-08-27 22:23:01 -0700

    [js][dfs] Clean up Possible Bipartition solution.

--- solutions/javascript/dfs/possibleBipartition.js
+++ solutions/javascript/dfs/possibleBipartition.js
@@ -1,18 +1,18 @@
-const NO_GROUP_ASSIGNED_YET = -1;
-const GROUP_ONE = 1;
-const GROUP_TWO = 2;
+const GROUP_A = -1;
+const NO_GROUP_ASSIGNED_YET = 0;
+const GROUP_B = 1;
 
-const buildGraph = (edges) => {
+const getOppositeGroup = (group) => -1 * group;
+
+const buildGraph = (numNodes, edges) => {
     const graph = {};
+
+    for (let node = 1; node <= numNodes; node += 1) {
+        graph[node] = [];
+    }
 
     for (const edge of edges) {
         const [nodeOne, nodeTwo] = edge;
-
-        const nodeOneInGraph = graph.hasOwnProperty(nodeOne);
-        const nodeTwoInGraph = graph.hasOwnProperty(nodeTwo);
-
-        if (!nodeOneInGraph) graph[nodeOne] = [];
-        if (!nodeTwoInGraph) graph[nodeTwo] = [];
 
         graph[nodeOne].push(nodeTwo);
         graph[nodeTwo].push(nodeOne);
@@ -21,53 +21,46 @@ const buildGraph = (edges) => {
     return graph;
 };
 
-const checkIfCanBeBipartite = (graph, node, groups, prevNodeGroup) => {
+const checkIfComponentIsBipartite = (graph, node, groupAssignments, prevNodeGroupAssignment) => {
     // Base cases
-    if (groups[node] !== NO_GROUP_ASSIGNED_YET) {
-        const hasConflict = groups[node] === prevNodeGroup;
-
-        return !hasConflict;
-    }
+    const hasAssignedGroup = groupAssignments[node] !== NO_GROUP_ASSIGNED_YET;
+    if (hasAssignedGroup)
+        return groupAssignments[node] === getOppositeGroup(prevNodeGroupAssignment);
 
     // Process node
-    if (prevNodeGroup === null || prevNodeGroup === GROUP_TWO)
-        groups[node] = GROUP_ONE;
-    else groups[node] = GROUP_TWO;
+    groupAssignments[node] = getOppositeGroup(prevNodeGroupAssignment);
 
-    // Handle neighbors
-    const nodeInGraph = graph.hasOwnProperty(node);
-    if (!nodeInGraph) return true;
-
+    // Recurse on neighbors
     const neighbors = graph[node];
     for (const neighbor of neighbors) {
-        const canBeBipartite = checkIfCanBeBipartite(
+        const neighborComponentIsBipartite = checkIfComponentIsBipartite(
             graph,
             neighbor,
-            groups,
-            groups[node],
+            groupAssignments,
+            groupAssignments[node],
         );
 
-        if (!canBeBipartite) return false;
+        if (!neighborComponentIsBipartite) return false;
     }
 
     return true;
 };
 
 const possibleBipartition = (numPeople, dislikes) => {
-    const graph = buildGraph(dislikes);
-    const groups = new Array(numPeople + 1).fill(NO_GROUP_ASSIGNED_YET);
+    const graph = buildGraph(numPeople, dislikes);
+    const groupAssignments = new Array(numPeople + 1).fill(NO_GROUP_ASSIGNED_YET);
 
-    for (let personId = 1; personId < numPeople; personId += 1) {
-        if (groups[personId] !== NO_GROUP_ASSIGNED_YET) continue;
+    for (let personId = 1; personId <= numPeople; personId += 1) {
+        const hasBeenAssignedToGroup = groupAssignments[personId] !== NO_GROUP_ASSIGNED_YET;
+        if (hasBeenAssignedToGroup) continue;
 
-        const canBeBipartite = checkIfCanBeBipartite(
+        const componentIsBipartite = checkIfComponentIsBipartite(
             graph,
             personId,
-            groups,
-            null,
+            groupAssignments,
+            GROUP_B,
         );
-
-        if (!canBeBipartite) return false;
+        if (!componentIsBipartite) return false;
     }
 
     return true;
