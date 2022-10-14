63a9e478db707eb28042c8ab054dc022ec9efe4e
Rohin Manvi <37752545+rohinmanvi@users.noreply.github.com>
2022-10-14 13:14:21 -0700

    added recursive mergeTwoLists

--- solutions/python/general-algos/linked-lists/merge_two_sorted_lists.py
+++ solutions/python/general-algos/linked-lists/merge_two_sorted_lists.py
@@ -21,3 +21,17 @@ class Solution:
             tail_of_merged_list.next = cur_node_in_list_two
 
         return sentinel.next
+
+class Solution:
+    def mergeTwoLists(self, list_one, list_two):
+        if not list_one:
+            return list_two
+        if not list_two:
+            return list_one
+            
+        if list_one.val < list_two.val:
+            list_one.next = self.mergeTwoLists(list_one.next, list_two)
+            return list_one
+        
+        list_two.next = self.mergeTwoLists(list_one, list_two.next)
+        return list_two
