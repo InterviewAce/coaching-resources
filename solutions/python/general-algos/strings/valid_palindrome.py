35a80e7d53540f16a185d93e1457071a8541c7e6
rohinmanvi <rohinmanvi@gmail.com>
2022-10-26 21:18:27 -0700

    cleaner

--- solutions/python/general-algos/strings/valid_palindrome.py
+++ solutions/python/general-algos/strings/valid_palindrome.py
@@ -1,13 +1,16 @@
 
 class Solution:
     def isPalindrome(self, string: str) -> bool:
-        string = re.sub(r'[^a-z0-9]', '', string.lower())
-
         left_idx = 0
         right_idx = len(string) - 1
 
         while left_idx < right_idx:
-            if string[left_idx] != string[right_idx]:
+            while left_idx < right_idx and not string[left_idx].isalnum():
+                left_idx += 1
+            while left_idx < right_idx and not string[right_idx].isalnum():
+                right_idx -= 1
+            
+            if string[left_idx].lower() != string[right_idx].lower():
                 return False
 
             left_idx += 1
