1b63f976d30afde7c3e4206abdc0539641fdec4c
rohinmanvi <rohinmanvi@gmail.com>
2022-10-26 21:05:33 -0700

    cleaner

--- solutions/python/general-algos/strings/longest_palindromic_substring.py
+++ solutions/python/general-algos/strings/longest_palindromic_substring.py
@@ -27,23 +27,19 @@ class Solution:
         longest_palindrome_end_idx = 0
 
         for i in range(len(string)):
-            odd_palindrome_indices = self.get_longest_palindrome_centered_at(
+            odd_start_idx, odd_end_idx = self.get_longest_palindrome_centered_at(
                 string,
                 i,
                 i,
             )
-            even_palindrome_indices = self.get_longest_palindrome_centered_at(
+            even_start_idx, even_end_idx = self.get_longest_palindrome_centered_at(
                 string,
                 i,
                 i + 1,
             )
 
-            odd_start_idx, odd_end_idx = odd_palindrome_indices
-            even_start_idx, even_end_idx = even_palindrome_indices
-
             odd_length = odd_end_idx - odd_start_idx + 1
             even_length = even_end_idx - even_start_idx + 1
-
             cur_longest_palindrome_length = longest_palindrome_end_idx - longest_palindrome_start_idx + 1
 
             if odd_length > cur_longest_palindrome_length and odd_length > even_length:
