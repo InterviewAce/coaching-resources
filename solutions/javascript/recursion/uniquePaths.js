3c78f45540246010a38b7b780eaca13077903aa2
shreshth <shreshthsrivastava2@gmail.com>
2022-10-28 22:02:54 -0700

    [js][py] Add alternative Unique Paths recursion solution.

--- solutions/javascript/recursion/uniquePaths.js
+++ solutions/javascript/recursion/uniquePaths.js
@@ -1,3 +1,6 @@
+/*
+Approach 1:
+*/
 const isInBounds = (row, col) => {
     const rowInBounds = row >= 0;
     const colInBounds = col >= 0;
@@ -25,4 +28,57 @@ const uniquePaths = (numRows, numCols) => {
     const targetCol = numCols - 1;
 
     return getNumWaysFromStartToTarget(targetRow, targetCol);
+};
+
+/*
+Approach 2:
+*Note: this approach pretty much does the same thing as approach 1, the code is just
+structured differently.
+*/
+const getNumWaysToGetFromPositionToTarget = (curPosition, targetPosition) => {
+    const { row: curPositionRow, col: curPositionCol } = curPosition;
+    const { row: targetRow, col: targetCol } = targetPosition;
+
+    const isInBounds = curPositionRow <= targetRow && curPositionCol <= targetCol;
+    if (!isInBounds) return 0;
+
+    const isTargetPosition = curPositionRow === targetRow && curPositionCol === targetCol;
+    if (isTargetPosition) return 1;
+
+    const downPosition = {
+        row: curPositionRow + 1,
+        col: curPositionCol,
+    };
+
+    const rightPosition = {
+        row: curPositionRow,
+        col: curPositionCol + 1,
+    };
+
+    const numWaysFromDownPositionToTarget = getNumWaysToGetFromPositionToTarget(
+        downPosition,
+        targetPosition,
+    );
+    const numWaysFromRightPositionToTarget = getNumWaysToGetFromPositionToTarget(
+        rightPosition,
+        targetPosition,
+    );
+
+    const numWaysFromCurPositionToTarget =
+        numWaysFromDownPositionToTarget + numWaysFromRightPositionToTarget;
+    return numWaysFromCurPositionToTarget;
+};
+
+const uniquePaths = (numRows, numCols) => {
+    const startPosition = {
+        row: 0,
+        col: 0,
+    };
+
+    const targetPosition = {
+        row: numRows - 1,
+        col: numCols - 1,
+    };
+
+    return getNumWaysToGetFromPositionToTarget(startPosition, targetPosition);
 };
--- solutions/python/recursion/unique_paths.py
+++ solutions/python/recursion/unique_paths.py
@@ -1,3 +1,6 @@
+"""
+Approach 1:
+"""
 def is_in_bounds(row, col):
     row_in_bounds = row >= 0
     col_in_bounds = col >= 0
@@ -27,3 +30,36 @@ class Solution:
 
         return self.get_num_ways_from_start_to_target(target_row, target_col)
     
+"""
+Approach 2:
+*Note: this approach pretty much does the same thing as approach 1, the code is just
+structured differently.
+"""
+class Solution:
+    def get_num_ways_from_position_to_target(self, cur_position, target_position):
+        cur_position_row, cur_position_col = cur_position
+        target_position_row, target_position_col = target_position
+
+        is_in_bounds = cur_position_row <= target_row and cur_position_col <= target_col
+        if not is_in_bounds:
+            return 0
+
+        is_target_position = cur_position_row == target_row and cur_position_col == target_col
+        if is_target_position:
+            return 1
+
+        down_position = (cur_position_row + 1, cur_position_col)
+        right_position = (cur_position_row, cur_position_col + 1)
+
+        num_ways_from_down_position_to_target = self.get_num_ways_from_position_to_target(down_position, target_position)
+        num_ways_from_right_position_to_target = self.get_num_ways_from_position_to_target(right_position, target_position)
+
+        num_ways_from_cur_position_to_target = num_ways_from_down_position_to_target + num_ways_from_right_position_to_target
+
+        return num_ways_from_cur_position_to_target
+
+    def uniquePaths(self, num_rows: int, num_cols: int) -> int:
+        start_position = (0, 0)
+        target_position = (num_rows - 1, num_cols - 1)
+
+        return self.get_num_ways_from_position_to_target(start_position, target_position)
