d8f163fe2bfe283a864c7f794484f4405b1274de
shreshth <shreshthsrivastava2@gmail.com>
2022-11-02 16:17:48 -0700

    [py][binary-search] Update Single Element in a Sorted Array.

--- solutions/python/binary-search/single_element_in_a_sorted_array.py
+++ solutions/python/binary-search/single_element_in_a_sorted_array.py
@@ -1,9 +1,10 @@
+NON_DUPLICATE_NOT_FOUND = -1
+
 def get_mid_idx(left_idx, right_idx):
     """
     Why do we use this math to find the middle index?
     Couldn't we just use
         mid_idx = (left_idx + right_idx) // 2
-
     The reason is integer overflow. In JavaScript, the maximum
     size of a number is (2^53) - 1. Suppose we used the above logic
     (i.e. `const midIdx = Math.floor((left + right) / 2);`). Let's
@@ -15,7 +16,6 @@ def get_mid_idx(left_idx, right_idx):
     So, when computing `left + right` we try to store (2^54) - 5,
     but JavaScript cannot handle such a large number. This problem
     is called 'integer overflow'.
-
     Instead, we use the approach you see below. Mathematically,
     it's doing the same thing. It just does so in a different order.
     This logic is saying 'find the difference between right and left.
@@ -44,6 +44,8 @@ class Solution:
 
         left_idx = 0
         right_idx = last_idx
+        
+        non_duplicate_idx = NON_DUPLICATE_NOT_FOUND
 
         while left_idx < right_idx:
             mid_idx = get_mid_idx(left_idx, right_idx)
@@ -51,7 +53,7 @@ class Solution:
             # Make sure `midIdx` points to the first element in the current pair.
             if is_odd(mid_idx):
                 mid_idx -= 1
-
+                
             # If we have a pair at midIdx and (midIdx + 1), then the non-duplicate is in the right half of the remaining section. So, we set `leftIdx = midIdx + 2` to skip past the current pair.
             mid_val = nums[mid_idx]
             next_val = nums[mid_idx + 1]
@@ -60,7 +62,8 @@ class Solution:
 
             # Otherwise, the non-duplicate must be in the left half of the remaining section (including `midIdx`).
             else:
+                non_duplicate_idx = mid_idx
                 right_idx = mid_idx
 
-        non_duplicate_num = nums[left_idx]
+        non_duplicate_num = nums[non_duplicate_idx]
         return non_duplicate_num
